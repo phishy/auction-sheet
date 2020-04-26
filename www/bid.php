5550a772ce613e615aaf3d105b29348e98abe81f
Jeff Loiselle <jeff@loiselles.com>
2020-04-25 20:45:18 -0500

    reorganizing

--- www/bid.php
+++ www/bid.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 include 'client.php';
 
 $sheet = @$_GET['sku'];
--- www/highest.php
+++ www/highest.php
@@ -1,13 +1,13 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 include 'client.php';
 
 $sheet = $_GET['sku'];
 $spreadsheetId = $_GET['id'];
 
 if (!$sheet || !$spreadsheetId) {
-  die('Missing id or sku');
+    die('Missing id or sku');
 }
 
 $client = getClient();
@@ -15,19 +15,15 @@ $service = new Google_Service_Sheets($client);
 
 $range = $sheet;
 $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-$values = $response->getValues();
+$values = (array) $response->getValues();
 
 $highest = null;
 
-if (empty($values)) {
-    print "No data found.\n";
-} else {
-    foreach ($values as $row) {
-      @list($first, $last, $email, $bid) = $row;
-      if ($bid > $highest) {
-        $highest = $bid;
-      }
-    }
+foreach ($values as $row) {
+  @list($first, $last, $email, $bid) = $row;
+  if ($bid > $highest) {
+      $highest = $bid;
+  }
 }
 
 print json_encode(['highestBid' => $highest]);
