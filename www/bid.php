ef289ef7b0c892633d94732c4d8fbf83d2f90a4e
Jeff Loiselle <jeff@loiselles.com>
2020-04-25 20:19:34 -0500

    solidifying

--- www/bid.php
+++ www/bid.php
@@ -3,8 +3,8 @@
 require __DIR__ . '/vendor/autoload.php';
 include 'client.php';
 
-$sheet = $_GET['sku'];
-$spreadsheetId = $_GET['id'];
+$sheet = @$_GET['sku'];
+$spreadsheetId = @$_GET['id'];
 
 if (!$sheet || !$spreadsheetId) {
   die('Missing id or sku');
--- www/script.js
+++ www/script.js
@@ -1,31 +1,46 @@
 $(function () {
-  let id = $("#bid").data("id");
-  let sku = $("#bid").data("sku");
+  let $form = $("#bid");
+  let id = $form.data("id");
+  let sku = $form.data("sku");
+  let refresh = parseInt($form.data("refresh"));
 
-  getHighestBid(id, sku);
+  renderHighestBid(id, sku);
 
-  setInterval(() => {
-    getHighestBid(id, sku);
-  }, 5000);
+  if (refresh) {
+    setInterval(() => {
+      renderHighestBid(id, sku);
+    }, refresh * 1000);
+  }
 
-  $("#bid").submit((e) => {
+  $form.submit((e) => {
+    e.preventDefault();
     $.ajax({
       method: "POST",
       url: `bid.php?id=${id}&sku=${sku}`,
-      data: getFormData($("#bid")),
+      data: formToJSON($("#bid")),
       contentType: "application/json; charset=utf-8",
       dataType: "json",
-    }).then(res => {
-      window.location.href = $('#bid').data('success');
-    });
-    return e.preventDefault();
+    })
+      .then((res) => {
+        if ($form.data("success")) {
+          window.location.href = $form.data("success");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        if ($form.data("error")) {
+          window.location.href = $form.data("error");
+        } else {
+          alert("An error occurred. Please try again.");
+        }
+      });
   });
 });
 
 /**
  * Turns form data into JSON
  */
-function getFormData($form) {
+function formToJSON($form) {
   var unindexed_array = $form.serializeArray();
   var indexed_array = {};
   $.map(unindexed_array, function (n, i) {
@@ -37,7 +52,7 @@ function getFormData($form) {
 /**
  * Fetches the highest bid from the server
  */
-function getHighestBid(id, sku) {
+function renderHighestBid(id, sku) {
   $.get(`highest.php?id=${id}&sku=${sku}`).then((res) => {
     let data = JSON.parse(res);
     var formatter = new Intl.NumberFormat("en-US", {
